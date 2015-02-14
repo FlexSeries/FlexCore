1a6c921ad1f442540d968e3fbe988f1bc8e9846d
Stealth2800 <stealth2800@stealthyone.com>
2015-02-14 00:36:44 -0800

    Removed unnecessary variable.

--- src/main/java/me/st28/flexseries/flexcore/hooks/TownyHook.java
+++ src/main/java/me/st28/flexseries/flexcore/hooks/TownyHook.java
@@ -1,12 +1,6 @@
 package me.st28.flexseries.flexcore.hooks;
 
-import com.palmergames.bukkit.towny.Towny;
-import com.palmergames.bukkit.towny.db.TownyDataSource;
-import com.palmergames.bukkit.towny.object.TownyUniverse;
-
 public final class TownyHook extends Hook {
-
-    private TownyUniverse townyUniverse;
 
     public TownyHook() {
         super("Towny");
@@ -14,20 +8,12 @@ public final class TownyHook extends Hook {
 
     @Override
     protected void handleEnable() {
-        townyUniverse = ((Towny) getPlugin()).getTownyUniverse();
+
     }
 
     @Override
     protected void handleDisable() {
-        townyUniverse = null;
-    }
 
-    public TownyUniverse getTownyUniverse() {
-        return townyUniverse;
-    }
-
-    public TownyDataSource getTownyDataSource() {
-        return TownyUniverse.getDataSource();
     }
 
 }
