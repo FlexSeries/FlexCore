c1424a41c43a1baebb131a6bc4564c8609eee2c6
Stealth2800 <stealth2800@stealthyone.com>
2015-02-14 00:37:42 -0800

    Added jobs hook.

---
+++ src/main/java/me/st28/flexseries/flexcore/hooks/JobsHook.java
@@ -0,0 +1,19 @@
+package me.st28.flexseries.flexcore.hooks;
+
+public final class JobsHook extends Hook {
+
+    public JobsHook() {
+        super("Jobs");
+    }
+
+    @Override
+    protected void handleEnable() {
+
+    }
+
+    @Override
+    protected void handleDisable() {
+
+    }
+
+}
