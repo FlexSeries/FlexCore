370b5cf962a2a492531f796da0ee0fe5f2bed702
Stealth2800 <stealth2800@stealthyone.com>
2015-02-23 21:14:10 -0800

    Added ActionBarAPI by ConnorLinfoot from Spigot.

---
+++ src/main/java/me/st28/flexseries/flexcore/utils/ActionBar.java
@@ -0,0 +1,23 @@
+package me.st28.flexseries.flexcore.utils;
+
+import net.minecraft.server.v1_8_R1.ChatSerializer;
+import net.minecraft.server.v1_8_R1.IChatBaseComponent;
+import net.minecraft.server.v1_8_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+/**
+ * v1.1.1 - http://www.spigotmc.org/resources/actionbarapi.1315/
+ */
+public final class ActionBar {
+
+    private ActionBar() { }
+
+    public static void sendActionBar(Player player, String message){
+        CraftPlayer p = (CraftPlayer) player;
+        IChatBaseComponent cbc = ChatSerializer.a("{\"text\": \"" + message + "\"}");
+        PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, (byte) 2);
+        p.getHandle().playerConnection.sendPacket(ppoc);
+    }
+
+}
