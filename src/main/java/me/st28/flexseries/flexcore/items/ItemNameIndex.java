632eb5ecf4d35f20b0250850bbe7d0e70be864ab
Stealth2800 <stealth2800@stealthyone.com>
2015-02-25 00:22:09 -0800

    Now uses 'MaterialRef' to store the damage value of the material as well.

--- src/main/java/me/st28/flexseries/flexcore/items/ItemNameIndex.java
+++ src/main/java/me/st28/flexseries/flexcore/items/ItemNameIndex.java
@@ -3,6 +3,7 @@ package me.st28.flexseries.flexcore.items;
 import me.st28.flexseries.flexcore.FlexCore;
 import me.st28.flexseries.flexcore.logging.LogHelper;
 import me.st28.flexseries.flexcore.plugins.FlexModule;
+import me.st28.flexseries.flexcore.utils.MaterialRef;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
@@ -13,7 +14,7 @@ import java.util.Map.Entry;
 
 public final class ItemNameIndex extends FlexModule<FlexCore> {
 
-    private final Map<String, Material> nameIndex = new HashMap<>();
+    private final Map<String, MaterialRef> nameIndex = new HashMap<>();
 
     public ItemNameIndex(FlexCore plugin) {
         super(plugin, "item_names", "Allows for easier item lookup based on name");
@@ -21,29 +22,48 @@ public final class ItemNameIndex extends FlexModule<FlexCore> {
 
     @Override
     protected void handleReload() {
+        nameIndex.clear();
         FileConfiguration config = getConfig();
 
-        ConfigurationSection itemSec = config.getConfigurationSection("items");
+        ConfigurationSection itemSec = config.getConfigurationSection("Items");
         if (itemSec == null) {
             return;
         }
 
         for (String rawMat : itemSec.getKeys(false)) {
+            String materialName;
             Material material;
+            short damage;
+
+            String[] split = rawMat.split(":");
+            if (split.length == 2) {
+                try {
+                    damage = Short.valueOf(split[1]);
+                } catch (NumberFormatException ex) {
+                    LogHelper.warning(this, "Invalid damage value '" + split[1] + "' in raw material '" + rawMat + "'");
+                    continue;
+                }
+            } else {
+                damage = 0;
+            }
+
+            materialName = split[0];
+
             try {
-                material = Material.valueOf(rawMat.toUpperCase());
+                material = Material.valueOf(materialName.toUpperCase());
             } catch (Exception ex) {
                 LogHelper.warning(this, "Invalid material '" + rawMat + "'");
                 continue;
             }
 
+            MaterialRef materialRef = new MaterialRef(material, damage);
             for (String alias : itemSec.getStringList(rawMat)) {
                 if (nameIndex.containsKey(alias.toLowerCase())) {
                     LogHelper.warning(this, "Material alias '" + alias + "' is already in use.");
                     continue;
                 }
 
-                nameIndex.put(alias.toLowerCase(), material);
+                nameIndex.put(alias.toLowerCase(), materialRef);
             }
         }
     }
@@ -51,12 +71,12 @@ public final class ItemNameIndex extends FlexModule<FlexCore> {
     /**
      * @return a material that matches the input name.
      */
-    public Material getMaterial(String name) {
+    public MaterialRef getMaterial(String name) {
         Validate.notNull(name, "Name cannot be null.");
 
         name = name.toLowerCase();
         List<String> found = new ArrayList<>();
-        for (Entry<String, Material> entry : nameIndex.entrySet()) {
+        for (Entry<String, MaterialRef> entry : nameIndex.entrySet()) {
             String key = entry.getKey();
 
             if (name.equals(key)) {
@@ -78,12 +98,12 @@ public final class ItemNameIndex extends FlexModule<FlexCore> {
     /**
      * @return a collection of available aliases for a given material.
      */
-    public Collection<String> getAliases(Material material) {
+    public Collection<String> getAliases(MaterialRef material) {
         Validate.notNull(material, "Material cannot be null.");
 
         List<String> aliases = new ArrayList<>();
-        for (Entry<String, Material> entry : nameIndex.entrySet()) {
-            if (entry.getValue() == material) {
+        for (Entry<String, MaterialRef> entry : nameIndex.entrySet()) {
+            if (entry.getValue().equals(material)) {
                 aliases.add(entry.getKey());
             }
         }
