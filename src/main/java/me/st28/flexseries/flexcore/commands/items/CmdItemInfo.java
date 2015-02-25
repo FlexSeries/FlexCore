5f9574d06c11c0954dbbab0e1aaf4762e2ff0bf3
Stealth2800 <stealth2800@stealthyone.com>
2015-02-25 00:23:44 -0800

    Now uses MaterialRef

--- src/main/java/me/st28/flexseries/flexcore/commands/items/CmdItemInfo.java
+++ src/main/java/me/st28/flexseries/flexcore/commands/items/CmdItemInfo.java
@@ -3,6 +3,7 @@ package me.st28.flexseries.flexcore.commands.items;
 import me.st28.flexseries.flexcore.FlexCore;
 import me.st28.flexseries.flexcore.commands.FlexCommand;
 import me.st28.flexseries.flexcore.commands.FlexCommandSettings;
+import me.st28.flexseries.flexcore.commands.FlexHelpCommand;
 import me.st28.flexseries.flexcore.commands.exceptions.CommandInterruptedException;
 import me.st28.flexseries.flexcore.items.ItemNameIndex;
 import me.st28.flexseries.flexcore.lists.ListBuilder;
@@ -10,8 +11,8 @@ import me.st28.flexseries.flexcore.messages.MessageReference;
 import me.st28.flexseries.flexcore.permissions.PermissionNodes;
 import me.st28.flexseries.flexcore.plugins.FlexPlugin;
 import me.st28.flexseries.flexcore.plugins.exceptions.ModuleDisabledException;
+import me.st28.flexseries.flexcore.utils.MaterialRef;
 import me.st28.flexseries.flexcore.utils.StringUtils;
-import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -25,11 +26,14 @@ public final class CmdItemInfo extends FlexCommand<FlexCore> {
     public CmdItemInfo(FlexCore plugin) {
         super(
                 plugin,
-                "flexitemdb",
+                "flexiteminfo",
                 new FlexCommandSettings<FlexCore>()
                     .permission(PermissionNodes.ITEM_INFO)
                     .setPlayerOnly()
+                    .helpPath("FlexCore.ItemInfo")
         );
+
+        registerSubcommand(new FlexHelpCommand<>(plugin, this));
     }
 
     @Override
@@ -40,7 +44,7 @@ public final class CmdItemInfo extends FlexCommand<FlexCore> {
             throw new CommandInterruptedException(MessageReference.create(FlexCore.class, "errors.must_hold_item"));
         }
 
-        Material type = item.getType();
+        MaterialRef type = new MaterialRef(item);
         List<String> aliases = new ArrayList<>();
         try {
             aliases.addAll(FlexPlugin.getRegisteredModule(ItemNameIndex.class).getAliases(type));
@@ -48,7 +52,7 @@ public final class CmdItemInfo extends FlexCommand<FlexCore> {
 
         ListBuilder builder = new ListBuilder("title", "Item Info", null, label);
 
-        builder.addMessage("title", "Type", type.toString() + ":" + item.getDurability());
+        builder.addMessage("title", "Type", type.toString());
         if (!aliases.isEmpty()) {
             builder.addMessage("title", "Aliases", StringUtils.stringCollectionToString(aliases, ", "));
         }
